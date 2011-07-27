f9e093a4e9d753fb8c61df1fb0301c598ed66099
Friedrich Lindenberg <friedrich@pudo.org>
2011-07-27 22:28:34 +0200

    break up code base into smaller files

---
+++ dateparser.py
@@ -0,0 +1,48 @@
+def create_date_formats(day_first=True):
+    """generate combinations of time and date formats with different delimeters"""
+
+    if day_first:
+        date_formats = "dd/mm/yyyy yyyy/mm/dd".split()
+        python_date_formats = "%d/%m/%Y %Y/%m/%d".split()
+    else:
+        date_formats = "mm/dd/yyyy yyyy/mm/dd".split()
+        python_date_formats = "%m/%d/%Y %Y/%m/%d".split()
+    both_date_formats = zip(date_formats, python_date_formats)
+
+    #time_formats = "hh:mmz hh:mm:ssz hh:mmtzd hh:mm:sstzd".split()
+    time_formats = "hh:mm:ssz hh:mm:sstzd".split()
+    python_time_formats = "%H:%M%Z %H:%M:%S%Z %H:%M%z %H:%M:%S%z".split()
+    both_time_fromats = zip(time_formats, python_time_formats)
+
+    #date_seperators = ["-","."," ","","/","\\"]
+    date_seperators = ["-",".","/"]
+
+    all_date_formats = []
+
+    for seperator in date_seperators:
+        for date_format, python_date_format in both_date_formats:
+            all_date_formats.append(
+                (
+                 date_format.replace("/", seperator),
+                 python_date_format.replace("/", seperator)
+                )
+            )
+
+    all_formats = {}
+
+    for date_format, python_date_format in all_date_formats:
+        all_formats[date_format] = python_date_format
+        for time_format, python_time_format in both_time_fromats:
+
+            all_formats[date_format + time_format] = \
+                    python_date_format + python_time_format
+
+            all_formats[date_format + "T" + time_format] =\
+                    python_date_format + "T" + python_time_format
+
+            all_formats[date_format + " " + time_format] =\
+                    python_date_format + " " + python_time_format
+    return all_formats
+
+DATE_FORMATS = create_date_formats()
+
--- messytables/csv_file.py
+++ messytables/csv_file.py
@@ -1,98 +1,14 @@
-import csv
 import re
-import codecs
+import csv
 import datetime
 import decimal
 import itertools
 from StringIO import StringIO
 
-## from python documentation
-class UTF8Recoder:
-    """
-    Iterator that reads an encoded stream and reencodes the input to UTF-8
-    """
-    def __init__(self, f, encoding):
-        self.reader = codecs.getreader(encoding)(f, 'ignore')
-    def __iter__(self):
-        return self
-
-    def next(self):
-        line = self.reader.readline()
-        if not line or line == '\0':
-            raise StopIteration
-        result = line.encode("utf-8")
-        return result
-
-class UnicodeReader:
-    """
-    A CSV reader which will iterate over lines in the CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
-        self.reader = csv.reader(f, dialect=dialect, **kwds)
-
-    def next(self):
-        row = self.reader.next()
-        self.line_num = self.reader.line_num
-        if not row:
-            raise StopIteration
-        return [s.decode("utf-8") for s in row]
-
-    def __iter__(self):
-        return self
-
-
-def create_date_formats(day_first=True):
-    """generate combinations of time and date formats with different delimeters"""
-
-    if day_first:
-        date_formats = "dd/mm/yyyy yyyy/mm/dd".split()
-        python_date_formats = "%d/%m/%Y %Y/%m/%d".split()
-    else:
-        date_formats = "mm/dd/yyyy yyyy/mm/dd".split()
-        python_date_formats = "%m/%d/%Y %Y/%m/%d".split()
-    both_date_formats = zip(date_formats, python_date_formats)
-
-    #time_formats = "hh:mmz hh:mm:ssz hh:mmtzd hh:mm:sstzd".split()
-    time_formats = "hh:mm:ssz hh:mm:sstzd".split()
-    python_time_formats = "%H:%M%Z %H:%M:%S%Z %H:%M%z %H:%M:%S%z".split()
-    both_time_fromats = zip(time_formats, python_time_formats)
-
-    #date_seperators = ["-","."," ","","/","\\"]
-    date_seperators = ["-",".","/"]
-
-    all_date_formats = []
-
-    for seperator in date_seperators:
-        for date_format, python_date_format in both_date_formats:
-            all_date_formats.append(
-                (
-                 date_format.replace("/", seperator),
-                 python_date_format.replace("/", seperator)
-                )
-            )
-
-    all_formats = {}
-
-    for date_format, python_date_format in all_date_formats:
-        all_formats[date_format] = python_date_format
-        for time_format, python_time_format in both_time_fromats:
-
-            all_formats[date_format + time_format] = \
-                    python_date_format + python_time_format
-
-            all_formats[date_format + "T" + time_format] =\
-                    python_date_format + "T" + python_time_format
-
-            all_formats[date_format + " " + time_format] =\
-                    python_date_format + " " + python_time_format
-    return all_formats
-
-DATE_FORMATS = create_date_formats()
-
-POSSIBLE_TYPES = ["int", "bool", "decimal"] + DATE_FORMATS.keys()
+from messytables.dateparser import DATE_FORMATS
+from messytables.unicsv import UnicodeReader
+from messytables.ordereddict import OrderedDict
+from messytables.type_guesser import TypeGuesser
 
 class CsvFile(object):
 
@@ -180,29 +96,6 @@ class CsvFile(object):
                 best_line = i
         self.skip_lines = best_line
         self.guessed_skip_lines = True
-
-    def skip_line_rows(self):
-
-        if not self.guessed_skip_lines or not self.skip_lines:
-            return []
-
-        if self.buffer:
-            flat_file = StringIO(self.buffer)
-        else:
-            flat_file = open(self.path, mode = "rb")
-        reader = codecs.getreader(self.encoding)(flat_file, 'ignore')
-
-        results = []
-        
-        for num, line in enumerate(reader):
-            result = {}
-            result.update(dict((h, None) for h in self.headings))
-            result["__errors"] = dict(error="skipped_line",
-                                      original_line=line)
-            results.append(result)
-
-        return results
-
 
     def get_dialect(self):
 
@@ -341,16 +234,12 @@ class CsvFile(object):
 
 
     def get_csv_reader(self):
-
         if self.buffer:
             flat_file = StringIO(self.buffer)
         else:
             flat_file = open(self.path, mode = "rb")
-
         csv_reader = UnicodeReader(flat_file, self.dialect, self.encoding)
-
         self.skip(csv_reader)
-
         return flat_file, csv_reader
 
 
@@ -469,176 +358,6 @@ class CsvFile(object):
 
         finally:
             flat_file.close()
-
-class TypeGuesser(object):
-
-    def __init__(self, iterable, guess_lines = 1000):
-        
-        self.iterable = iterable
-        self.guess_lines = guess_lines
-
-
-    def guess(self):
-
-        possible_types = set(POSSIBLE_TYPES)
-
-        max_length = 0
-
-        for num, value in enumerate(self.iterable):
-            #if len(line) != len(self.headings):
-            #    continue
-            max_length = max(max_length, len(value))
-            if not value:
-                continue
-            for type in list(possible_types):
-                if type == "int":
-                    if not self.is_int(value):
-                        possible_types.remove("int")
-                elif type == "bool":
-                    if not self.is_bool(value):
-                        possible_types.remove("bool")
-                elif type == "decimal":
-                    if not self.is_decimal(value):
-                        possible_types.remove("decimal")
-                else:
-                    python_format = DATE_FORMATS[type]
-                    if not self.is_date_format(value, python_format):
-                        possible_types.remove(type)
-
-
-            if num > self.guess_lines:
-                check = self.check_possible_types(possible_types)
-                if possible_types == set():
-                    break
-                elif check:
-                    return check
-
-        if not possible_types:
-            return min(max_length * 7, 2000)
-        return self.check_possible_types(possible_types)
-
-    def is_int(self, val):
-
-        try:
-            val = int(val)
-            if val > 1000000000000:
-                return False
-            return True
-        except ValueError:
-            return False
-
-    def is_decimal(self, val):
-        try:
-            val =  decimal.Decimal(val)
-            if val > 1000000000000:
-                return False
-            return True
-        except decimal.InvalidOperation:
-            decimal.InvalidOperation
-            return False
-
-    def is_bool(self, val):
-        if val.lower() in "1 true 0 false".split():
-            return True
-        return False
-
-    def is_date_format(self, val, date_format):
-        try:
-            date = datetime.datetime.strptime(val, date_format)
-            if date.year > 3000:
-                return False
-            return True
-        except ValueError:
-            return False
-
-    def check_possible_types(self, possible_types):
-
-        if (len(possible_types) == 3 and
-            "int" in possible_types and
-            "decimal" in possible_types):
-            possible_types.remove("int")
-            possible_types.remove("decimal")
-        if (len(possible_types) == 2 and
-            "decimal" in possible_types):
-            possible_types.remove("decimal")
-        if 'bool' in possible_types:
-            return 'bool'
-        if len(possible_types) == 2:
-            if not (set(possible_types) - set(DATE_FORMATS)):
-                return possible_types.pop()
-        if len(possible_types) == 1:
-            return possible_types.pop()
-
-## {{{ http://code.activestate.com/recipes/576669/ (r18)
-## Raymond Hettingers proporsal to go in 2.7
-from collections import MutableMapping
-
-class OrderedDict(dict, MutableMapping):
-
-    # Methods with direct access to underlying attributes
-
-    def __init__(self, *args, **kwds):
-        if len(args) > 1:
-            raise TypeError('expected at 1 argument, got %d', len(args))
-        if not hasattr(self, '_keys'):
-            self._keys = []
-        self.update(*args, **kwds)
-
-    def clear(self):
-        del self._keys[:]
-        dict.clear(self)
-
-    def __setitem__(self, key, value):
-        if key not in self:
-            self._keys.append(key)
-        dict.__setitem__(self, key, value)
-
-    def __delitem__(self, key):
-        dict.__delitem__(self, key)
-        self._keys.remove(key)
-
-    def __iter__(self):
-        return iter(self._keys)
-
-    def __reversed__(self):
-        return reversed(self._keys)
-
-    def popitem(self):
-        if not self:
-            raise KeyError
-        key = self._keys.pop()
-        value = dict.pop(self, key)
-        return key, value
-
-    def __reduce__(self):
-        items = [[k, self[k]] for k in self]
-        inst_dict = vars(self).copy()
-        inst_dict.pop('_keys', None)
-        return (self.__class__, (items,), inst_dict)
-
-    # Methods with indirect access via the above methods
-
-    setdefault = MutableMapping.setdefault
-    update = MutableMapping.update
-    pop = MutableMapping.pop
-    keys = MutableMapping.keys
-    values = MutableMapping.values
-    items = MutableMapping.items
-
-    def __repr__(self):
-        pairs = ', '.join(map('%r: %r'.__mod__, self.items()))
-        return '%s({%s})' % (self.__class__.__name__, pairs)
-
-    def copy(self):
-        return self.__class__(self)
-
-    @classmethod
-    def fromkeys(cls, iterable, value=None):
-        d = cls()
-        for key in iterable:
-            d[key] = value
-        return d
-## end of http://code.activestate.com/recipes/576669/ }}}
 
 
 
