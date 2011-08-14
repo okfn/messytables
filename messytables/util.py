893124eacb4fd3e0e1134174f7713e24d8cfdef1
Friedrich Lindenberg <friedrich@pudo.org>
2011-08-14 16:49:05 +0200

    Transform skip-lines function to a proper processor to be operated on row sets.

--- messytables/util.py
+++ messytables/util.py
@@ -7,10 +7,13 @@ FLOAT = float
 DECIMAL = decimal.Decimal
 DATE = datetime
 
-
-
-def skip_n(iterable, offset):
-    """ Skip ``offset`` from ``iterable``. """
-    zip(xrange(offset), iterable)
-    for item in iterable:
-        yield item
+def offset_processor(offset):
+    """ Skip ``offset`` from the given iterator. This can 
+    be used in combination with the ``headers_processor`` to 
+    apply the result of a header scan to the table. """
+    i = [0]
+    def apply_offset(row_set, row):
+        if i[0] >= offset:
+            return row
+        i[0] += 1
+    return apply_offset
