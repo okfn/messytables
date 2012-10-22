49bcd5dda8cff77ed0a2db36e3af2eee2cee9fe9
Joshua Tauberer <jt@occams.info>
2012-10-22 12:19:48 +0000

    in the new AnyTable, dont treat text/plain as necessarily CSV but instead fall back to the file extension if provided as a hint as to whether it is CSV or TSV; also some code cleanup

--- messytables/any.py
+++ messytables/any.py
@@ -6,6 +6,16 @@ from messytables import CSVTableSet, XLSTableSet, XLSXTableSet, ZIPTableSet
 class AnyTableSet(TableSet):
     """ Reads any supported table type according to a specified MIME type or file extension or automatically detecting the type by using the magic library which looks at the first few bytes of the file."""
 
+    @staticmethod
+    def make_stream_seekable(fileobj):
+        try:
+            fileobj.seek(0)
+            # if we got here, the stream is seekable
+        except:
+            # otherwise seek failed, so slurp in stream and wrap it in a StringIO
+            fileobj = StringIO.StringIO(fileobj.read())
+        return fileobj
+
     @classmethod
     def from_fileobj(cls, fileobj, mimetype=None, extension=None):
         """ Opens whatever sort of file is passed in, using the MIME type (e.g mimetype='text/csv') or file extension
@@ -14,10 +24,7 @@ class AnyTableSet(TableSet):
         if mimetype == None:
             # Since we need to peek the start of the stream, make sure we can
             # seek back later. If not, slurp in the contents into a StringIO.
-            try:
-                fileobj.seek(0)
-            except:
-                fileobj = StringIO.StringIO(fileobj.read())
+            fileobj = make_stream_seekable(fileobj)
             header = fileobj.read(1024)
             mimetype = magic.from_buffer(header, mime=True)
             fileobj.seek(0)
@@ -27,9 +34,9 @@ class AnyTableSet(TableSet):
             # files, so don't check them before we check for a ZIP file.
             return ZIPTableSet.from_fileobj(fileobj)
             
-        if mimetype in ('text/csv', 'text/plain', 'text/comma-separated-values') or (extension and extension.lower() in ('csv',)):
+        if mimetype in ('text/csv', 'text/comma-separated-values') or (extension and extension.lower() in ('csv',)):
             return CSVTableSet.from_fileobj(fileobj, delimiter=',')
-        if mimetype in ('text/tsv', 'text/plain', 'text/tab-separated-values') or (extension and extension.lower() in ('tsv',)):
+        if mimetype in ('text/tsv', 'text/tab-separated-values') or (extension and extension.lower() in ('tsv',)):
             return CSVTableSet.from_fileobj(fileobj, delimiter='\t')
         if mimetype in ('application/ms-excel', 'application/vnd.ms-excel', 'application/xls') or (extension and extension.lower() in ('xls',)):
             return XLSTableSet.from_fileobj(fileobj)
