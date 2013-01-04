3fbc7fa9f3da9ae013bd16a9cdbaf1c4acfd3d10
Dominik Moritz <domoritz@gmail.com>
2013-01-04 13:59:03 +0100

    use assert_equal

--- test/test_rowset.py
+++ test/test_rowset.py
@@ -32,51 +32,51 @@ class ReadTest(unittest.TestCase):
         fh = horror_fobj('simple.csv')
         table_set = CSVTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
-        assert 7 == len(list(row_set))
+        assert_equal(7, len(list(row_set)))
         row = list(row_set.sample)[0]
-        assert row[0].value == 'date'
-        assert row[1].value == 'temperature'
+        assert_equal(row[0].value, 'date')
+        assert_equal(row[1].value, 'temperature')
 
         for row in list(row_set):
-            assert 3 == len(row), row
-            assert row[0].type == StringType()
+            assert_equal(3, len(row))
+            assert_equal(row[0].type, StringType())
 
     def test_read_complex_csv(self):
         fh = horror_fobj('complex.csv')
         table_set = CSVTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
-        assert 4 == len(list(row_set))
+        assert_equal(4, len(list(row_set)))
         row = list(row_set.sample)[0]
-        assert row[0].value == 'date'
-        assert row[1].value == 'another date'
-        assert row[2].value == 'temperature'
-        assert row[3].value == 'place'
+        assert_equal(row[0].value, 'date')
+        assert_equal(row[1].value, 'another date')
+        assert_equal(row[2].value, 'temperature')
+        assert_equal(row[3].value, 'place')
 
         for row in list(row_set):
-            assert 4 == len(row), row
-            assert row[0].type == StringType()
+            assert_equal(4, len(row))
+            assert_equal(row[0].type, StringType())
 
     def test_read_simple_tsv(self):
         fh = horror_fobj('example.tsv')
         table_set = CSVTableSet.from_fileobj(fh, delimiter='\t')
         row_set = table_set.tables[0]
-        assert 141 == len(list(row_set)), len(list(row_set))
+        assert_equal(141, len(list(row_set)))
         row = list(row_set.sample)[0]
-        assert row[0].value == 'hour', row[0].value
-        assert row[1].value == 'expr1_0_imp', row[1].value
+        assert_equal(row[0].value, 'hour')
+        assert_equal(row[1].value, 'expr1_0_imp')
         for row in list(row_set):
-            assert 17 == len(row), len(row)
-            assert row[0].type == StringType()
+            assert_equal(17, len(row))
+            assert_equal(row[0].type, StringType())
 
     def test_read_simple_xls(self):
         fh = horror_fobj('simple.xls')
         table_set = XLSTableSet.from_fileobj(fh)
-        assert 1 == len(table_set.tables)
+        assert_equal(1, len(table_set.tables))
         row_set = table_set.tables[0]
         row = list(row_set.sample)[0]
-        assert row[0].value == 'date'
-        assert row[1].value == 'temperature'
-        assert row[2].value == 'place'
+        assert_equal(row[0].value, 'date')
+        assert_equal(row[1].value, 'temperature')
+        assert_equal(row[2].value, 'place')
 
         for row in list(row_set):
             assert 3 == len(row), row
@@ -84,12 +84,12 @@ class ReadTest(unittest.TestCase):
     def test_read_simple_xlsx(self):
         fh = horror_fobj('simple.xlsx')
         table_set = XLSXTableSet.from_fileobj(fh)
-        assert 1 == len(table_set.tables)
+        assert_equal(1, len(table_set.tables))
         row_set = table_set.tables[0]
         row = list(row_set.sample)[0]
-        assert row[0].value == 'date'
-        assert row[1].value == 'temperature'
-        assert row[2].value == 'place'
+        assert_equal(row[0].value, 'date')
+        assert_equal(row[1].value, 'temperature')
+        assert_equal(row[2].value, 'place')
 
         for row in list(row_set):
             assert 3 == len(row), row
@@ -100,35 +100,35 @@ class ReadTest(unittest.TestCase):
         row_set = table_set.tables[0]
         offset, headers = headers_guess(row_set.sample)
         assert 5 == len(headers), headers
-        assert u'Region' == headers[1].strip(), headers[1]
+        assert_equal(u'Region', headers[1].strip())
         row_set.register_processor(headers_processor(headers))
         row_set.register_processor(offset_processor(offset + 1))
         for row in row_set:
-            assert 5 == len(row), row
+            assert_equal(5, len(row))
 
     def test_read_head_offset_csv(self):
         fh = horror_fobj('simple.csv')
         table_set = CSVTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
         offset, headers = headers_guess(row_set.sample)
-        assert offset == 0, offset
+        assert_equal(offset, 0)
         row_set.register_processor(offset_processor(offset + 1))
         data = list(row_set.sample)
-        assert int(data[0][1].value) == 1, data[0][1].value
+        assert_equal(int(data[0][1].value), 1)
         data = list(row_set)
-        assert int(data[0][1].value) == 1, data[0][1].value
+        assert_equal(int(data[0][1].value), 1)
 
     def test_read_head_offset_excel(self):
         fh = horror_fobj('simple.xls')
         table_set = XLSTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
         offset, headers = headers_guess(row_set.sample)
-        assert offset == 0, offset
+        assert_equal(offset, 0)
         row_set.register_processor(offset_processor(offset + 1))
         data = list(row_set.sample)
-        assert int(data[0][1].value) == 1, data[0][1].value
+        assert_equal(int(data[0][1].value), 1)
         data = list(row_set)
-        assert int(data[0][1].value) == 1, data[0][1].value
+        assert_equal(int(data[0][1].value), 1)
 
     def test_guess_headers(self):
         fh = horror_fobj('weird_head_padding.csv')
@@ -138,8 +138,7 @@ class ReadTest(unittest.TestCase):
         row_set.register_processor(headers_processor(headers))
         row_set.register_processor(offset_processor(offset + 1))
         data = list(row_set)
-        assert 'Chirurgie' in data[9][0].value, \
-            data[9][0].value
+        assert 'Chirurgie' in data[9][0].value, data[9][0].value
 
         fh = horror_fobj('weird_head_padding.csv')
         table_set = CSVTableSet.from_fileobj(fh)
@@ -147,8 +146,7 @@ class ReadTest(unittest.TestCase):
         row_set.register_processor(headers_processor(['foo', 'bar']))
         data = list(row_set)
         assert 'foo' in data[12][0].column, data[12][0]
-        assert 'Chirurgie' in data[12][0].value, \
-            data[12][0].value
+        assert 'Chirurgie' in data[12][0].value, data[12][0].value
 
     def test_read_type_guess_simple(self):
         fh = horror_fobj('simple.csv')
@@ -156,14 +154,14 @@ class ReadTest(unittest.TestCase):
         row_set = table_set.tables[0]
         types = type_guess(row_set.sample)
         expected_types = [DateType("%Y-%m-%d"), IntegerType(), StringType()]
-        assert types == expected_types, types
+        assert_equal(types, expected_types)
 
         row_set.register_processor(types_processor(types))
         data = list(row_set)
         header_types = map(lambda c: c.type, data[0])
-        assert header_types == [StringType()] * 3, header_types
+        assert_equal(header_types, [StringType()] * 3)
         row_types = map(lambda c: c.type, data[2])
-        assert expected_types == row_types, row_types
+        assert_equal(expected_types, row_types)
 
     def test_read_type_know_simple(self):
         fh = horror_fobj('simple.xls')
@@ -171,22 +169,22 @@ class ReadTest(unittest.TestCase):
         row_set = table_set.tables[0]
         row = list(row_set.sample)[1]
         types = [c.type for c in row]
-        assert types == [DateType(None), IntegerType(), StringType()], types
+        assert_equal(types, [DateType(None), IntegerType(), StringType()])
 
     def test_read_encoded_csv(self):
         fh = horror_fobj('utf-16le_encoded.csv')
         table_set = CSVTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
-        assert 328 == len(list(row_set)), len(list(row_set))
+        assert_equal(328, len(list(row_set)))
         row = list(row_set.sample)[0]
-        assert row[1].value == 'Organisation_name', row[1].value
+        assert_equal(row[1].value, 'Organisation_name')
 
     def test_long_csv(self):
         fh = horror_fobj('long.csv')
         table_set = CSVTableSet.from_fileobj(fh)
         row_set = table_set.tables[0]
         data = list(row_set)
-        assert 4000 == len(data), len(data)
+        assert_equal(4000, len(data))
 
 
 class TypeGuessTest(unittest.TestCase):
@@ -201,7 +199,7 @@ class TypeGuessTest(unittest.TestCase):
         rows = CSVTableSet(csv_file).tables[0]
         guessed_types = type_guess(rows.sample)
 
-        assert guessed_types == [DecimalType(), DateType('%Y/%m/%d'), IntegerType(), DateType('%d %B %Y')], guessed_types
+        assert_equal(guessed_types, [DecimalType(), DateType('%Y/%m/%d'), IntegerType(), DateType('%d %B %Y')])
 
     def test_type_guess_strict(self):
         import locale
@@ -213,7 +211,7 @@ class TypeGuessTest(unittest.TestCase):
             4,   2012/2/12, 42,"-2,000",24 October 2012,"42"''')
         rows = CSVTableSet(csv_file).tables[0]
         guessed_types = type_guess(rows.sample, strict=True)
-        assert guessed_types == [StringType(), StringType(), DecimalType(), IntegerType(), DateType('%d %B %Y'), FloatType()], guessed_types
+        assert_equal(guessed_types, [StringType(), StringType(), DecimalType(), IntegerType(), DateType('%d %B %Y'), FloatType()])
 
     def test_guessing_does_padding(self):
         csv_file = StringIO.StringIO('''
@@ -222,13 +220,13 @@ class TypeGuessTest(unittest.TestCase):
             foo, , 1500''')
         rows = CSVTableSet(csv_file).tables[0]
         guessed_types = type_guess(rows.sample, strict=True)
-        assert len(guessed_types) == 3
-        assert guessed_types == [StringType(), StringType(), DecimalType()], guessed_types
+        assert_equal(len(guessed_types), 3)
+        assert_equal(guessed_types, [StringType(), StringType(), DecimalType()])
 
 
 class TestStreamInput(unittest.TestCase):
     @httprettified
-    def test_http_resource(self):
+    def test_http_csv(self):
         url = 'http://www.messytables.org/static/long.csv'
         HTTPretty.register_uri(HTTPretty.GET, url,
             body=horror_fobj('long.csv').read(),
