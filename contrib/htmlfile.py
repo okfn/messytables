2e95e45c6f0b4f3d0ddf2546fc84badd7464b571
Dragon Dave <dragon@scraperwiki.com>
2013-05-29 17:48:37 +0100

    BROKEN: tried to implement colspan/rowspan

--- contrib/htmlfile.py
+++ contrib/htmlfile.py
@@ -1,6 +1,7 @@
 from messytables.core import RowSet, TableSet, Cell
 import lxml.html
 import json
+from collections import Counter
 
 
 class HTMLTableSet(TableSet):
@@ -19,6 +20,15 @@ class HTMLTableSet(TableSet):
     def tables(self):
         return [HTMLRowSet(json.dumps(dict(x.attrib)), x) for x in self.htmltables]
 
+def skipcols(row, skip):
+    """
+    >>> skipcols([1,2,3],[1,2,3])
+    [1, None, None, None, 2, 3]
+    """
+    for i in skip:
+        row.insert(i,Cell("NOPE"))
+    return row
+
 
 class HTMLRowSet(RowSet):
     def __init__(self, name, sheet, window=None):
@@ -28,8 +38,24 @@ class HTMLRowSet(RowSet):
         super(HTMLRowSet, self).__init__()
 
     def raw(self, sample=False):
+        toskip = Counter()
         # TODO handle rowspan/colspan gracefully.
         for row in self.sheet.xpath('.//tr'):
             # TODO: handle header nicer - preserve the fact it's a header!
-            yield [Cell(cell.text_content())
-                   for cell in row.xpath('.//*[name()="td" or name()="th"]')]
+            htmlcells = row.xpath('.//*[name()="td" or name()="th"]')
+            items = [Cell(cell.text_content()) for cell in htmlcells]
+            column = 0
+            """ at the end of this chunk, you will have an accurate toskip."""
+            # Add rowspan(or 1) to the next colspan(or 1) columns.
+            # Except the current column; add one less to that.
+            for htmlcell in htmlcells:
+                while column in toskip.keys():
+                    column = column+1
+                rowspan = int(htmlcell.attrib.get('rowspan', "1"))
+                colspan = int(htmlcell.attrib.get('colspan', "1"))
+                skipdict = Counter({x: rowspan for x in range(column, column + colspan)})  # fullbox
+                skipdict.subtract(Counter([column]))  # remove top left cell
+                toskip = toskip + skipdict  # this also normalises
+            yield skipcols(items, toskip.keys())
+            toskip.subtract(toskip.keys())
+            toskip = toskip + Counter()  # normalise: remove <=0
--- contrib/test_rowset.py
+++ contrib/test_rowset.py
@@ -14,7 +14,7 @@ from htmlfile import HTMLTableSet
 
 
 class ReadTest(unittest.TestCase):
-    def test_read_simple_csv(self):
+    def test_read_html(self):
         fh = horror_fobj('html.html')
         table_set = HTMLTableSet(fh)
         row_set = table_set.tables[0]
@@ -22,3 +22,10 @@ class ReadTest(unittest.TestCase):
         row = list(row_set.sample)[0]
         assert_equal(row[0].value.strip(), 'HDI Rank')
         assert_equal(row[1].value.strip(), 'Country')
+
+    def test_read_span_html(self):
+        fh = open("/home/dragon/kitten.html", 'r')
+        table_set = HTMLTableSet(fh)
+        row_set = table_set.tables[0]
+        for i in row_set:
+            print i
