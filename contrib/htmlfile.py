b23dd785fba588e8ab9552c1dc05bc6604c3c8ac
Dragon Dave <dragon@scraperwiki.com>
2013-06-03 14:00:58 +0100

    Seems to be working

--- contrib/htmlfile.py
+++ contrib/htmlfile.py
@@ -1,7 +1,7 @@
 from messytables.core import RowSet, TableSet, Cell
 import lxml.html
 import json
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 class HTMLTableSet(TableSet):
@@ -20,13 +20,18 @@ class HTMLTableSet(TableSet):
     def tables(self):
         return [HTMLRowSet(json.dumps(dict(x.attrib)), x) for x in self.htmltables]
 
+
 def skipcols(row, skip):
     """
     >>> skipcols([1,2,3],[1,2,3])
-    [1, None, None, None, 2, 3]
+    [1, <Cell(String:>, <Cell(String:>, <Cell(String:>, 2, 3]
+    >>> skipcols([1,2,4],[2])
+    [1, 2, <Cell(String:>, 4]
     """
+    print "***", skip
     for i in skip:
-        row.insert(i,Cell("NOPE"))
+        if i >= 0:  # TODO: negative hack!
+            row.insert(i, Cell(""))  # TODO: option to use top-left of col/rowspan.
     return row
 
 
@@ -38,9 +43,10 @@ class HTMLRowSet(RowSet):
         super(HTMLRowSet, self).__init__()
 
     def raw(self, sample=False):
-        toskip = Counter()
+        toskip = defaultdict(list)
+        assert "Element" not in repr(toskip), toskip
         # TODO handle rowspan/colspan gracefully.
-        for row in self.sheet.xpath('.//tr'):
+        for r, row in enumerate(self.sheet.xpath('.//tr')):
             # TODO: handle header nicer - preserve the fact it's a header!
             htmlcells = row.xpath('.//*[name()="td" or name()="th"]')
             items = [Cell(cell.text_content()) for cell in htmlcells]
@@ -49,13 +55,22 @@ class HTMLRowSet(RowSet):
             # Add rowspan(or 1) to the next colspan(or 1) columns.
             # Except the current column; add one less to that.
             for htmlcell in htmlcells:
-                while column in toskip.keys():
+                assert type(r) == int
+                while column in toskip[r]:
                     column = column+1
                 rowspan = int(htmlcell.attrib.get('rowspan', "1"))
                 colspan = int(htmlcell.attrib.get('colspan', "1"))
-                skipdict = Counter({x: rowspan for x in range(column, column + colspan)})  # fullbox
-                skipdict.subtract(Counter([column]))  # remove top left cell
-                toskip = toskip + skipdict  # this also normalises
-            yield skipcols(items, toskip.keys())
-            toskip.subtract(toskip.keys())
-            toskip = toskip + Counter()  # normalise: remove <=0
+                x_range = range(column, column + colspan)
+                y_range = range(r, r + rowspan)
+                if rowspan + colspan > 2:
+                    print "span at %r, %r: %r, %r"%(r, column, rowspan, colspan)
+                for x in x_range:
+                    for y in y_range:
+                        if column != x or r != y:
+                            print "blank %r, %r"%(x,y)
+                            toskip[y].append(x)
+                            print toskip
+
+                column = column + 1
+            
+            yield skipcols(items, toskip[r])
--- contrib/test_rowset.py
+++ contrib/test_rowset.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
-import os
-import unittest
-
-from nose.tools import assert_equal
-
-
-def horror_fobj(name):
-    fn = os.path.join(os.path.dirname(__file__), '..', 'horror', name)
-    return open(fn, 'rb')
-
+#import os
+#import unittest
+#
+#from nose.tools import assert_equal
+#
+#
+#def horror_fobj(name):
+#    fn = os.path.join(os.path.dirname(__file__), '..', 'horror', name)
+#    return open(fn, 'rb')
+#
 from messytables import *
 from htmlfile import HTMLTableSet
 
 
-class ReadTest(unittest.TestCase):
-    def test_read_html(self):
-        fh = horror_fobj('html.html')
-        table_set = HTMLTableSet(fh)
-        row_set = table_set.tables[0]
-        assert_equal(200, len(list(row_set)))
-        row = list(row_set.sample)[0]
-        assert_equal(row[0].value.strip(), 'HDI Rank')
-        assert_equal(row[1].value.strip(), 'Country')
+#class ReadTest(unittest.TestCase):
+#    def test_read_html(self):
+#        fh = horror_fobj('html.html')
+#        table_set = HTMLTableSet(fh)
+#        row_set = table_set.tables[0]
+#        assert_equal(200, len(list(row_set)))
+#        row = list(row_set.sample)[0]
+#        assert_equal(row[0].value.strip(), 'HDI Rank')
+#        assert_equal(row[1].value.strip(), 'Country')
 
-    def test_read_span_html(self):
-        fh = open("/home/dragon/kitten.html", 'r')
-        table_set = HTMLTableSet(fh)
-        row_set = table_set.tables[0]
-        for i in row_set:
-            print i
+
+
+def test_read_span_html(self):
+    fh = open("/home/dragon/kitten.html", 'r')
+    table_set = HTMLTableSet(fh)
+    row_set = table_set.tables[0]
+    for i in row_set:
+        print i
+
+test_read_span_html(None)
