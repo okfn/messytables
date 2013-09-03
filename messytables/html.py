5b05270ad96024ddd50f371224f2b7e915c9e8a8
Dragon Dave <dragon@scraperwiki.com>
2013-09-03 12:46:45 +0100

    revert old properties

--- messytables/html.py
+++ messytables/html.py
@@ -27,8 +27,8 @@ class HTMLTableSet(TableSet):
                     self.htmltables.append(t)
                     t.attrib['messytable'] = 'done'
                     dropped = True
-            assert dropped, "Found no tables not containing other tables.\
-                             This is a bug."  # avoid infinite loops
+            assert dropped, "Didn't find any tables not containing " + \
+                "other tables. This is a bug."  # avoid infinite loops
 
     @property
     def tables(self):
@@ -91,12 +91,12 @@ class HTMLRowSet(RowSet):
             html_cells = self.in_table(
                 row.xpath('.//*[name()="td" or name()="th"]'))
 
-            # at the end of this chunk, you will have accurate blank_cells
+            """ at the end of this chunk, you have accurate blank_cells."""
             output_column = 0
             for html_cell in html_cells:
                 assert type(r) == int
                 while output_column in blank_cells[r]:
-                    output_column += 1  # pass over col, not in source table
+                    output_column += 1  # pass over col, doesn't exist in src
                 rowspan = int(html_cell.attrib.get('rowspan', "1"))
                 colspan = int(html_cell.attrib.get('colspan', "1"))
                 x_range = range(output_column, output_column + colspan)
--- test/test_read.py
+++ test/test_read.py
@@ -2,7 +2,6 @@
 import unittest
 
 from . import horror_fobj
-from nose.tools import assert_equal, assert_in
 from nose.tools import assert_equal, assert_true
 try:
     # Python 2.6 doesn't provide assert_is_instance
@@ -18,8 +17,6 @@ from messytables import (CSVTableSet, StringType, HTMLTableSet,
                          IntegerType, rowset_as_jts,
                          types_processor, type_guess)
 import datetime
-
-import lxml.html
 
 
 class ReadCsvTest(unittest.TestCase):
@@ -400,16 +397,6 @@ class ReadHtmlTest(unittest.TestCase):
         assert_equal(row[0].value.strip(), 'HDI Rank')
         assert_equal(row[1].value.strip(), 'Country')
         assert_equal(row[4].value.strip(), '2010')
-
-    def test_read_html_property(self):
-        fh = horror_fobj('html.html')
-        table_set = HTMLTableSet(fh)
-        row_set = table_set.tables[0]
-        row = list(row_set.sample)[0]
-        assert_in('</th>', row[4].properties['html'])
-        for cell in row:
-            if 'span' not in cell.properties:
-                lxml.html.fromstring(cell.properties['html'])
 
     def test_read_span_html(self):
         fh = horror_fobj('rowcolspan.html')
