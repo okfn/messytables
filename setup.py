2235ac96c9c07241a16128d6f80f2859e640482d
Dragon Dave McKee <dragon@scraperwiki.com>
2015-02-05 17:41:55 +0000

    0.14.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ See the full documentation at: http://messytables.readthedocs.org
 
 setup(
     name='messytables',
-    version='0.14.1',
+    version='0.14.2',
     description="Parse messy tabular data in various formats",
     long_description=long_desc,
     classifiers=[
