b795f14bcf4aaed01af7b6d1bf4b3b066d6dbe1c
Dragon Dave McKee <dragon@scraperwiki.com>
2015-04-27 14:20:23 +0000

    test_stream

--- messytables/compat23.py
+++ messytables/compat23.py
@@ -1,8 +1,12 @@
 import sys
 PY2 = sys.version_info[0] == 2
-if not PY2:
-    native_string = str
-    string_types = (str,)
-else:
+if PY2:
+    import urllib2
     native_string = unicode
     string_types = (str, unicode)
+    urlopen = urllib2.urlopen
+else:  # i.e. PY3
+    import urllib.request
+    native_string = str
+    string_types = (str,)
+    urlopen = urllib.request.urlopen
