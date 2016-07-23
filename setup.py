059ace7b8216e34469d295b49d0534fdd03bfe3c
Friedrich Lindenberg <friedrich@pudo.org>
2016-07-23 14:24:17 +0200

    fix up test errors, attempt to make travis pass

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'xlrd>=0.8.0',
         'python-magic>=0.4.6',  # used for file type guessing
         'chardet>=2.3.0',
-        'python-dateutil>=1.5.0',
         'lxml>=3.2',
         'requests>=2.0',
         'html5lib',
